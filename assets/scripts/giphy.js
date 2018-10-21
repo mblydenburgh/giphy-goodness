66952afb20d05eb66257d89f25342232b0526fb4
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-21 11:46:52 -0400

    side bar content

--- assets/scripts/giphy.js
+++ assets/scripts/giphy.js
@@ -1,1 +1,25 @@
 //api key rpelFBXi3lzv7VO6rXM08B9lJFN1a7aT
+//define jQuery Selectors
+const buttonsSelector = $(`#butons`);
+
+
+let giphyMaker = {
+    characters: ['dennis reynolds', 'frank reynolds', 'deandra reynolds', 'charlie kelly', 'mac mcdonald'],
+
+    printButtons: function() {
+        this.characters.map(character=>{this.makeButton(character)});
+    },
+
+    makeButton:function(character){
+        
+    },
+
+    getData: function () {
+        return `coming soon!`;
+    },
+}
+
+//display initial list of fictional characters
+$(document).ready(function () {
+    giphyMaker.printButtons();
+})
