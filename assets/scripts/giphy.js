97cfab137b783a70436465ba220f257c63d76428
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-21 12:44:26 -0400

    submit button adds a new search button based on text input value

--- assets/scripts/giphy.js
+++ assets/scripts/giphy.js
@@ -1,17 +1,35 @@
 //api key rpelFBXi3lzv7VO6rXM08B9lJFN1a7aT
+
 //define jQuery Selectors
-const buttonsSelector = $(`#butons`);
+const buttonsSection = $(`#buttons`);
+const searchButton = $(`#search-button`);
+const searchTextBox = $(`#search-box`);
+
+//define global variables
+let searchString;
 
 
 let giphyMaker = {
     characters: ['dennis reynolds', 'frank reynolds', 'deandra reynolds', 'charlie kelly', 'mac mcdonald'],
 
-    printButtons: function() {
-        this.characters.map(character=>{this.makeButton(character)});
+    printButtons: function () {
+        buttonsSection.empty();
+        this.characters.map(character => { this.makeButton(character) });
     },
 
-    makeButton:function(character){
-        
+    makeButton: function (character) {
+        //console.log(`making button with ${character}`);
+        let newButton = $(`<button>`);
+        newButton.text(character);
+        buttonsSection.append(newButton);
+    },
+
+    addButton: function (searchString) {
+        if (!this.characters.includes(searchString)) {
+            this.characters.push(searchString);
+            this.printButtons(searchString);
+        }
+
     },
 
     getData: function () {
@@ -22,4 +40,11 @@ let giphyMaker = {
 //display initial list of fictional characters
 $(document).ready(function () {
     giphyMaker.printButtons();
+
+    searchButton.click(function () {
+        searchString = searchTextBox.val();
+        //console.log(searchString);
+        giphyMaker.addButton(searchString);
+    })
+
 })
