75f7c4869618e3a1859a9565669abec371d5b5b6
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-22 18:02:48 -0400

    clicking a gif animates it, clicking again will set the img back to static

--- assets/scripts/giphy.js
+++ assets/scripts/giphy.js
@@ -18,7 +18,7 @@ let giphyMaker = {
         this.characters.map(character => { this.makeButton(character) });
 
         //with buttons created, have event listener to serve imgs
-        $(`.character-button`).click(function(){
+        $(`.character-button`).click(function () {
             console.log(`passing ${this.innerHTML} to getData`);
             giphyMaker.getData(this.innerHTML);
         })
@@ -41,35 +41,46 @@ let giphyMaker = {
     getData: function (search) {
         $.ajax({
             url: `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}&limit=10`,
-            method:'GET'
-        }).done(function(response){
+            method: 'GET'
+        }).done(function (response) {
             console.log(response);
             giphyMaker.printImages(response.data);
         })
     },
 
-    printImages: function(dataArray){
-        console.log(`dataArray: ${dataArray}`);
-        dataArray.map(img=>{this.makeImage(img)});
+    printImages: function (dataArray) {
+        //console.log(`dataArray: ${dataArray}`);
+        dataArray.map(img => { this.makeImage(img) });
 
-        $(`img`).click(function(){
-            console.log(this.data(`anim`));
-            //event.attr("src",this.attr("anim-url"));
+        $(`img`).click(function (event) {
+            //console.log(event.target.attributes);
+            let staticUrl = event.target.attributes[2].value;
+            let animUrl = event.target.attributes[3].value;
+            /*console.log(`static url: ${staticUrl}
+animated url: ${animUrl}`);*/
+            if (this.src === staticUrl) {
+                //make animate
+                this.src = animUrl;
+            } else {
+                //make static
+                this.src = staticUrl;
+            }
+            //this.src = animUrl;
         })
 
     },
 
-    makeImage: function(image){
+    makeImage: function (image) {
         let newImageDiv = $(`<div class="card">`);
         newImageDiv.html(`
 
         <img src="${image.images.fixed_height_still.url}" class="card-img-top" data-static="${image.images.fixed_height_still.url}" data-anim="${image.images.fixed_height.url}">
     <div class="card-body"><p class="card-text">url: ${image.url}<br>Giphy Score: ${image._score} <br> Rating: ${image.rating}</p></div>
         `);
-        imagesView.prepend(newImageDiv);  
+        imagesView.prepend(newImageDiv);
     },
 
-    clearData: function(){ //reset the characters list back to its default values;
+    clearData: function () { //reset the characters list back to its default values;
         this.characters = ['dennis reynolds', 'frank reynolds', 'deandra reynolds', 'charlie kelly', 'mac mcdonald'];
         this.printButtons();
     }
